7d6b380ad1b0454499d6a259887a80d3f588cf89
DESKTOP-QUST204\nqwrt <nqwrt@ymail.com>
2022-07-11 23:33:08 +0900

    first commit

---
+++ src/main/java/edu/hi/ex/SpringBootHjsLookflixApplication.java
@@ -0,0 +1,13 @@
+package edu.hi.ex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHjsLookflixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHjsLookflixApplication.class, args);
+	}
+
+}
